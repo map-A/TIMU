0729f3309f70165375212801297df9d6d6af3732
Zepeng Wang <591627025@qq.com>
2021-08-07 12:08:40 +0800

    add some new problem solution

--- Leetcode/p256.cpp
+++ Leetcode/p256.cpp
@@ -3,14 +3,21 @@
 using namespace std;
 
 int minCost(vector<vector<int>>& costs) {
-
-
-
-
+    const int N = 101;
+    int dp[N][3];
+    memset(dp,0, sizeof(dp));
+    for(int i=1;i<=costs.size();i++){
+        dp[i][0] = min({dp[i-1][1]+costs[i-1][1],dp[i-1][2]+costs[i-1][2]});
+        dp[i][1] = min({dp[i-1][0]+costs[i-1][0],dp[i-1][2]+costs[i-1][2]});
+        dp[i][2] = min({dp[i-1][0]+costs[i-1][0],dp[i-1][1]+costs[i-1][1]});
+    }
+    return min({dp[costs.size()][0],dp[costs.size()][1],dp[costs.size()][2]});
 }
 
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    vector<vector<int>>costs = {{7,6,2}};
+    int ret = minCost(costs);
+
     return 0;
 }
